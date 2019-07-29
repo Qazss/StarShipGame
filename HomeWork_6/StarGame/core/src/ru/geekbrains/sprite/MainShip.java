8c6275831b8f7ef0494a0d4570725bb3edab9815
dvoretskiy.aleksandr <dvoretskiy.aleksandr@otr.ru>
2019-07-29 10:11:15 +0300

    ДЗ к шестому уроку

---
+++ HomeWork_6/StarGame/core/src/ru/geekbrains/sprite/MainShip.java
@@ -0,0 +1,145 @@
+package ru.geekbrains.sprite;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.base.Ship;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.pool.BulletPool;
+
+public class MainShip extends Ship {
+
+    private static final int INVALID_POINTER = -1;
+
+    private boolean pressedLeft = false;
+    private boolean pressedRight = false;
+
+    private int leftPointer = INVALID_POINTER;
+    private int rightPointer = INVALID_POINTER;
+
+    public MainShip(TextureAtlas atlas, BulletPool bulletPool) {
+        super(atlas.findRegion("main_ship"), 1, 2, 2);
+        shootSound = Gdx.audio.newSound(Gdx.files.internal("sounds/laser.wav"));
+        bulletRegion = atlas.findRegion("bulletMainShip");
+        this.bulletPool = bulletPool;
+        reloadInterval = 0.2f;
+        v = new Vector2();
+        v0 = new Vector2(0.5f, 0);
+        bulletV = new Vector2(0f, 0.5f);
+        bulletHeight = 0.01f;
+        damage = 1;
+        hp = 10;
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        setHeightProportion(0.15f);
+        setBottom(worldBounds.getBottom() + 0.05f);
+    }
+
+    @Override
+    public void update(float delta) {
+        pos.mulAdd(v, delta);
+        reloadTimer += delta;
+        if (reloadTimer >= reloadInterval) {
+            reloadTimer = 0f;
+            shoot();
+        }
+        if (getLeft() > worldBounds.getRight()) {
+            setLeft(worldBounds.getLeft());
+        }
+        if (getRight() < worldBounds.getLeft()) {
+            setRight(worldBounds.getRight());
+        }
+    }
+
+    public boolean keyDown(int keycode) {
+        switch (keycode) {
+            case Input.Keys.LEFT:
+            case Input.Keys.A:
+                moveLeft();
+                pressedLeft = true;
+                break;
+            case Input.Keys.RIGHT:
+            case Input.Keys.D:
+                moveRight();
+                pressedRight = true;
+                break;
+        }
+        return false;
+    }
+
+    public boolean keyUp(int keycode) {
+        switch (keycode) {
+            case Input.Keys.LEFT:
+            case Input.Keys.A:
+                if (!pressedRight) {
+                    stop();
+                }
+                pressedLeft = false;
+                break;
+            case Input.Keys.RIGHT:
+            case Input.Keys.D:
+                if (!pressedLeft) {
+                    stop();
+                }
+                pressedRight = false;
+                break;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        if (touch.x < worldBounds.pos.x) {
+            if (leftPointer != INVALID_POINTER) {
+                return false;
+            }
+            leftPointer = pointer;
+            moveLeft();
+        } else {
+            if (rightPointer != INVALID_POINTER) {
+                return false;
+            }
+            rightPointer = pointer;
+            moveRight();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(Vector2 touch, int pointer, int button) {
+        if (pointer == leftPointer) {
+            leftPointer = INVALID_POINTER;
+            if (rightPointer != INVALID_POINTER) {
+                moveRight();
+            } else {
+                stop();
+            }
+        } else if (pointer == rightPointer) {
+            rightPointer = INVALID_POINTER;
+            if (leftPointer != INVALID_POINTER) {
+                moveLeft();
+            } else {
+                stop();
+            }
+        }
+        return false;
+    }
+
+    private void moveRight() {
+        v.set(v0);
+    }
+
+    private void moveLeft() {
+        v.set(v0).rotate(180);
+    }
+
+    private void stop() {
+        v.setZero();
+    }
+
+}
